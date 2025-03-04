da5d1f3db211517f1c6ece609daa9683af968fa0
Parmida Saemiyan <parrmida@gmail.com>
2025-03-04 15:50:00 -0800

    add r3f-perf to monitor performance

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { useControls } from 'leva'
+import { Perf } from "r3f-perf";
 import "./App.css";
 import Scene from "./scene";
 import Glow from "./glow";
@@ -8,6 +10,7 @@ import CameraController from "./CameraController";
 
 createRoot(document.getElementById("root")).render(
   <Canvas camera={{ position: [0, 3, 7.5], fov: 50 }}>
+    <Perf position="top-left" />
     <CameraController /> 
     <ambientLight intensity={Math.PI / 2} />
     <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
